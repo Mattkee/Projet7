8b965f4af51800881fd8bce33a8eae508db8a3d2
Mattkee <tkee@hotmail.fr>
2018-06-10 22:59:42 +0200

    add contraint on storyboard and create new swift file Calculate

--- Model/Calculate.swift
+++ Model/Calculate.swift
@@ -8,3 +8,49 @@
 
 import Foundation
 
+class Calculate {
+    // MARK: - Properties
+    var stringNumbers: [String] = [String()]
+    var operators: [String] = ["+"]
+    
+    // MARK: - Methods
+    func addNewNumber(_ newNumber: Int) {
+        if let stringNumber = stringNumbers.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumbers[stringNumbers.count-1] = stringNumberMutable
+        }
+    }
+    
+    func calculText() -> String {
+        var text = ""
+        for (number, stringNumber) in stringNumbers.enumerated() {
+            // Add operator
+            if number > 0 {
+                text += operators[number]
+            }
+            // Add number
+            text += stringNumber
+        }
+        return text
+    }
+    
+    func calculateTotal() -> Int {
+        var total = 0
+        for (enumerated, stringNumber) in stringNumbers.enumerated() {
+            if let number = Int(stringNumber) {
+                if operators[enumerated] == "+" {
+                    total += number
+                } else if operators[enumerated] == "-" {
+                    total -= number
+                }
+            }
+        }
+        return total
+    }
+    
+    func clear() {
+        stringNumbers = [String()]
+        operators = ["+"]
+    }
+}
