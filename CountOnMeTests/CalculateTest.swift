342ef2d44658c29a742d8e0a2ab50ed54bd72cd2
Mattkee <tkee@hotmail.fr>
2018-06-11 01:15:58 +0200

    create CountOnMeTests and add new file CalculateTest

---
+++ CountOnMeTests/CalculateTest.swift
@@ -0,0 +1,13 @@
+//
+//  CalculateTest.swift
+//  CountOnMeTests
+//
+//  Created by Lei et Matthieu on 11/06/2018.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import XCTest
+
+class CalculateTest: XCTestCase {
+    
+}
